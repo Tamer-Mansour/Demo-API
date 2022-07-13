61e68a7373a0d6363077a460ea285cb9484c9bf6
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-07-13 11:02:00 +0300

    add-useFormk-to-update-page

--- src/newUI/UpdateOne.jsx
+++ src/newUI/UpdateOne.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
 import {useLocation} from 'react-router-dom'
-import {FormControl, FormHelperText, Input, InputLabel} from '@mui/material';
-
 import {Grid, TextField, Button, Typography, Paper} from '@mui/material';
-import {Formik} from "formik";
+import {useFormik} from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+    breed: yup
+        .string('Enter your the breed name')
+        .required('Breed is required'),
+    coat: yup
+        .string('Enter your the coat name')
+        .required('Coat is required'),
+    country: yup
+        .string('Enter your the country name')
+        .required('Country is required'),
+    origin: yup
+        .string('Enter your the origin name')
+        .required('Origin is required'),
+    pattern: yup
+        .string('Enter your the pattern name')
+        .required('Pattern is required'),
+});
+
 
 const UpdateOne = ({catData}) => {
     const location = useLocation()
     console.log(location)
-    const object = location.state
+    const oldData = location.state
+
+    const formik = useFormik({
+        initialValues: {
+            breed: (oldData.breed),
+            coat: (oldData.coat),
+            country: (oldData.country),
+            origin: (oldData.origin),
+            pattern: (oldData.pattern),
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
 
     return (<Grid item xs={12} style={{maxWidth: 450}}>
         <Paper sx={{p: 6, display: 'flex', flexDirection: 'column'}}>
@@ -17,124 +50,77 @@ const UpdateOne = ({catData}) => {
                     Update Page
                 </Typography>
                 <Grid>
-                    <Formik
-                        initialValues={{breed: '', coat: '', country: '', origin: '', pattern: ''}}
-                        validate={values => {
-                            const errors = {};
-                            if (!values.breed) {
-                                errors.breed = 'Required';
-                            } else if (!values.coat) {
-                                errors.coat = 'Required';
-                            } else if (!values.country) {
-                                errors.country = 'Required';
-                            } else if (!values.origin) {
-                                errors.origin = 'Required';
-                            } else if (!values.pattern) {
-                                errors.pattern = 'Required';
-                            }
-                            return errors;
-                        }}
-                        onSubmit={(values, {setSubmitting}) => {
-                            setTimeout(() => {
-                                alert(JSON.stringify(values, null, 2));
-                                setSubmitting(false);
-                            }, 400);
-                        }}
-                    >
-                        {({
-                              values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, /* and other goodies */
-                          }) => (<form onSubmit={handleSubmit}>
-                            {/*breed*/}
-                            <FormControl>
-                                <InputLabel htmlFor="breed">Breed: {object.breed}</InputLabel>
-                                <Input
-                                    id="breed"
-                                    type="text"
-                                    name="breed"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.breed}
-                                    aria-describedby="err-breed"
-                                    placeholder={object.breed}
-                                />
-                                <FormHelperText
-                                    id="err-breed">{errors.breed && touched.breed && errors.breed}</FormHelperText>
-                            </FormControl>
-                            {/*  coat  */}
-                            <p></p>
-                            <FormControl>
-                                <InputLabel htmlFor="coat">Coat: {object.coat}</InputLabel>
-                                <Input
-                                    id="coat"
-                                    type="text"
-                                    name="coat"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.coat}
-                                    aria-describedby="err-coat"
-                                    placeholder={object.coat}
-                                />
-                                <FormHelperText
-                                    id="err-coat">{errors.coat && touched.coat && errors.coat}</FormHelperText>
-                            </FormControl>
-                            {/*  country  */}
-                            <p></p>
-                            <FormControl>
-                                <InputLabel htmlFor="country">Country: {object.country}</InputLabel>
-                                <Input
-                                    id="country"
-                                    type="text"
-                                    name="country"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.country}
-                                    aria-describedby="err-country"
-                                    placeholder={object.country}
-                                />
-                                <FormHelperText
-                                    id="err-country">{errors.country && touched.country && errors.country}</FormHelperText>
-                            </FormControl>
-                            {/*  origin  */}
-                            <p></p>
-                            <FormControl>
-                                <InputLabel htmlFor="origin">Origin: {object.origin}</InputLabel>
-                                <Input
-                                    id="origin"
-                                    type="text"
-                                    name="origin"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.origin}
-                                    aria-describedby="err-origin"
-                                    placeholder={object.origin}
-                                />
-                                <FormHelperText
-                                    id="err-origin">{errors.origin && touched.origin && errors.origin}</FormHelperText>
-                            </FormControl>
-                            {/*  pattern  */}
-                            <p></p>
-                            <FormControl>
-                                <InputLabel htmlFor="pattern">Pattern: {object.pattern}</InputLabel>
-                                <Input
-                                    id="pattern"
-                                    type="text"
-                                    name="pattern"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.pattern}
-                                    aria-describedby="err-pattern"
-                                    placeholder={object.pattern}
-                                />
-                                <FormHelperText
-                                    id="err-pattern">{errors.pattern && touched.pattern && errors.pattern}</FormHelperText>
-                            </FormControl>
-                            {/*    Button*/}
-                            <p></p>
-                            <div>
-                                <Button type="submit" disabled={isSubmitting} variant="outlined">Submit</Button>
-                            </div>
-                        </form>)}
-                    </Formik>
+                    <form onSubmit={formik.handleSubmit}>
+                        {/*breed*/}
+                        <TextField
+                            fullWidth
+                            id="breed"
+                            label={"breed - " + oldData.breed}
+                            type="text"
+                            name="breed"
+                            value={formik.values.breed}
+                            onChange={formik.handleChange}
+                            error={formik.touched.breed && Boolean(formik.errors.breed)}
+                            helperText={formik.touched.breed && formik.errors.breed}
+                        />
+
+                        {/*  coat  */}
+                        <p></p>
+                        <TextField
+                            fullWidth
+                            id="coat"
+                            label={"coat - " + oldData.coat}
+                            type="text"
+                            name="coat"
+                            value={formik.values.coat}
+                            onChange={formik.handleChange}
+                            error={formik.touched.coat && Boolean(formik.errors.coat)}
+                            helperText={formik.touched.coat && formik.errors.coat}
+                        />
+                        {/*  country  */}
+                        <p></p>
+                        <TextField
+                            fullWidth
+                            id="country"
+                            label={"country - " + oldData.country}
+                            type="text"
+                            name="country"
+                            value={formik.values.country}
+                            onChange={formik.handleChange}
+                            error={formik.touched.country && Boolean(formik.errors.country)}
+                            helperText={formik.touched.country && formik.errors.country}
+                        />
+                        {/*  origin  */}
+                        <p></p>
+                        <TextField
+                            fullWidth
+                            id="origin"
+                            label={"origin - " + oldData.origin}
+                            type="text"
+                            name="origin"
+                            value={formik.values.origin}
+                            onChange={formik.handleChange}
+                            error={formik.touched.origin && Boolean(formik.errors.origin)}
+                            helperText={formik.touched.origin && formik.errors.origin}
+                        />
+                        {/*  pattern  */}
+                        <p></p>
+                        <TextField
+                            fullWidth
+                            id="pattern"
+                            label={"pattern - " + oldData.pattern}
+                            type="text"
+                            name="pattern"
+                            value={formik.values.pattern}
+                            onChange={formik.handleChange}
+                            error={formik.touched.pattern && Boolean(formik.errors.pattern)}
+                            helperText={formik.touched.pattern && formik.errors.pattern}
+                        />
+                        <p></p>
+                        <div>
+                            <Button color="primary" variant="contained" fullWidth type="submit">Submit</Button>
+                        </div>
+                    </form>
                 </Grid>
             </div>
         </Paper>
